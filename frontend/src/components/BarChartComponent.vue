86384d72d1597f0fbbdd25d62a08ef193d9183f0
ejedkins <70499594+ejedkins@users.noreply.github.com>
2022-11-18 21:40:31 -0600

    Added Bar Chart Vue Component

---
+++ frontend/src/components/BarChartComponent.vue
@@ -0,0 +1,48 @@
+
+<template>
+  <canvas ref="myChart"></canvas>
+</template>
+
+<script>
+import { Chart, registerables } from 'chart.js'
+
+Chart.register(...registerables);
+export default {
+  props: {
+    label: {
+      type: Array,
+    },
+    chartData: {
+      type: Array,
+    },
+  },
+  async mounted() {
+    //console.log(this.chartData);
+    await new Chart(this.$refs.myChart, {
+      type: "bar",
+      data: {
+        labels: this.label,
+        datasets: [
+          {
+            label: "Clients per Event",
+            data: this.chartData,
+            backgroundColor: "rgba(144,238,144 , 0.9 )",
+
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        scale: {
+          yAxes: [{
+            ticks: {
+                fixedStepSize: 1
+            }
+        }],
+        }
+      }
+      
+    });
+  },
+};
+</script>
